ae5d1e8539ec6ff718e0273b7596b883c942be11
Amanuel Tewolde <github@ticklespace.com>
2011-06-25 20:26:50 -0400

    add copyright statements
    using MIT.

--- html5-qrcode.js
+++ html5-qrcode.js
@@ -1,3 +1,12 @@
+//---------------------------------------------------------------------
+// JavaScript-HTML5 QRCode Generator
+//
+// Copyright (c) 2011 Amanuel Tewolde
+//
+// Licensed under the MIT license:
+//   http://www.opensource.org/licenses/mit-license.php
+//
+//---------------------------------------------------------------------
 
 // Generates a QRCode of text provided.
 // First QRCode is rendered to a canvas.
